aa6183a5f8df67af3bf216abd0135c280d9fcda5
MadalenaAntunes <162154816+MadalenaAntunes@users.noreply.github.com>
2024-07-24 16:26:25 +0100

    fullstack dev

--- src/components/FullStackDev.jsx
+++ src/components/FullStackDev.jsx
@@ -3,24 +3,26 @@ import Image from "next/image";
 
 const FullStackDev = () => {
   return (
-    <div>
-      <div>
-        <h3>Hi I am</h3>
-        <h2>Madalena Antunes</h2>
-        <h1>Full Stack</h1>
-        <h1>Developer</h1>
-        <p>
+    <div className={styles.main}>
+      <div className={styles.text}>
+        <h3 className={styles.h3}>Hi I am</h3>
+        <h2 className={styles.h2}>
+          Madalena <b className={styles.h2_b}>Antunes</b>
+        </h2>
+        <h1 className={styles.h1_1}>Full Stack</h1>
+        <h1 className={styles.h1_2}>Developer</h1>
+        <p className={styles.p}>
           Lorem ipsum dolor sit amet consectetur. Tristique amet sed massa nibh
           lectus netus in. Aliquet donec morbi convallis pretium. Turpis tempus
           pharetra
         </p>
         {/* <HireMeBtn/> */}
       </div>
-      <div>
-        <div>
+      <div className={styles.image}>
+        <div className={styles.ball}>
           <Image src="/ball.png" alt="temporary" width={320} height={320} />
         </div>
-        <div>
+        <div className={styles.socials}>
           <i>insta</i>
           <i>linkedin</i>
           <i>github</i>
